7407a8e041a4c8d309927250e0f67427bf9357fa
Dom Leone <ohheydom@gmail.com>
2014-03-20 13:39:18 -0400

    Cleaned up code, factored out some code into additional methods.

--- vigenere-cipher.rb
+++ vigenere-cipher.rb
@@ -10,28 +10,30 @@
 #   -----------------
 #   ksgdgbjqbeqkklgdg
 #
-#  Using a 0 based alphabet (A=0), R is the 17th letter of the alphabet and T is the 19th letter of the alphabet. (17 + 19) mod 26 = 11 
-#  which is where K resides in the alphabet. Repeat for each key/message letter combination until done.
+#  Using a 0 based alphabet (A=0), R is the 17th letter of the alphabet and
+#  T is the 19th letter of the alphabet. (17 + 19) mod 26 = 11 which is where
+#  K resides in the alphabet. Repeat for each key/message letter combination
+#  until done.
 #
-#  Today's problem of the day is two part. The first part is to implement a Vigenère cipher in the programming language of your choice. 
-#  Feel free to post solutions or links to solutions in the comments.
+#  Today's problem of the day is two part. The first part is to implement
+#  a Vigenère cipher in the programming language of your choice. Feel free
+#  to post solutions or links to solutions in the comments.
 #
-#  The second part is to try and implement something to crack the message below (the key is 5 or less characters).
+#  The second part is to try and implement something to crack the message
+#  below (the key is 5 or less characters).
 #
 #  zejfokhtmsrmelcpodwhcgaw
 
 class VigenereCipher
-  
   LETTERS = ('a'..'z').to_a
   ONLY_LETTERS_GSUB = /[^a-zA-Z]/
-  DICTIONARY_FILE = "words" #UNIX has a great dictionary file located at /usr/share/dict/words
+  DICTIONARY_FILE = 'words' # UNIX has a great dictionary file located at /usr/share/dict/words
 
-  def initialize(key=nil, message=nil)
+  def initialize(key = nil, message = nil)
     @key = key ? key.gsub(ONLY_LETTERS_GSUB, '').downcase : nil
     @message = message ? message.gsub(ONLY_LETTERS_GSUB, '').downcase : nil
   end
 
-  #Setter methods with validation
   [:key, :message].each do |attr|
     define_method("#{attr}=") do |val|
       @code = nil if instance_variable_get("@#{attr}")
@@ -46,7 +48,7 @@ class VigenereCipher
       if @message && @code
         @key = VigenereCipher.get_message_or_key(@message, @code)
       else
-        @key = please_enter("key")
+        @key = please_enter('key')
       end
     end
   end
@@ -60,14 +62,14 @@ class VigenereCipher
       elsif @code
         VigenereCipher.crack(@code)
       else
-        @message = please_enter("message")
+        @message = please_enter('message')
       end
     end
   end
 
   def code=(code)
     if @key && @message
-      puts "You cannot modify the code. If you are trying to crack a code without a key or message, use VigenereCipher.crack(code)"
+      puts 'You cannot modify the code. If you are trying to crack a code without a key or message, use VigenereCipher.crack(code)'
     else
       @code = code.gsub(ONLY_LETTERS_GSUB, '')
     end
@@ -77,16 +79,15 @@ class VigenereCipher
     if @code || @key && @message
       @code
     elsif @key.nil? && @message.nil?
-      @key = please_enter("key")
-      @message = please_enter("message")
+      @key = please_enter('key')
+      @message = please_enter('message')
     elsif @message.nil?
-      @message = please_enter("message")
+      @message = please_enter('message')
     elsif @key.nil?
-      @key = please_enter("key")
+      @key = please_enter('key')
     end
 
     @code ||= VigenereCipher.get_code(@key, @message)
-
   end
 
   def clear
@@ -99,37 +100,45 @@ class VigenereCipher
     puts "Please enter a #{variable_name}: "
     gets.chomp.gsub(ONLY_LETTERS_GSUB, '').downcase
   end
-  
-  class << self
 
+  class << self
   def get_code(key, message)
     key = cycle(key, message).chars
-    key.each_index.each_with_object("") { |ind, obj| obj << LETTERS[(LETTERS.index(key[ind]) + LETTERS.index(message[ind])) % 26] }
+    key.each_index.each_with_object('') { |ind, obj| obj << LETTERS[(LETTERS.index(key[ind]) + LETTERS.index(message[ind])) % 26] }
   end
-
 
   def get_message_or_key(key, code)
     key = cycle(key, code).chars
-    key.each_index.each_with_object("") { |ind, obj| obj << LETTERS[(LETTERS.index(code[ind]) % 26) - (LETTERS.index(key[ind]))] }
+    key.each_index.each_with_object('') { |ind, obj| obj << LETTERS[(LETTERS.index(code[ind]) % 26) - (LETTERS.index(key[ind]))] }
   end
 
   def cycle(key, message)
-    new_key = ""
-  
-    key.chars.cycle do |letter| 
+    new_key = ''
+
+    key.chars.cycle do |letter|
       new_key << letter
       break if new_key.length == message.length
     end
-  
+
     new_key
   end
 
   def words_and_messages(code, dictionary, max_key_size)
-    dictionary.words_by_size(3,max_key_size).each_with_object({}) { |word, obj| obj[word] = get_message_or_key(word, code) }
+    dictionary.words_by_size(3, max_key_size).each_with_object({}) do |word, obj| 
+      obj[word] = get_message_or_key(word, code)
+    end
+  end
+
+  def check_values_proc(keys_and_msgs, reg, num, poss_keys_and_msgs)
+    proc do |word|
+      keys_and_msgs.values.grep(/^#{word}#{reg * num}$/).each do |val|
+        poss_keys_and_msgs[keys_and_msgs.key(val)] ||= val
+      end
+    end
   end
 
   def crack(code, max_key_size)
-    puts "Cracking..."
+    puts 'Cracking...'
 
     dictionary = Dictionary.new(DICTIONARY_FILE)
     all_words = dictionary.all_words.dup
@@ -139,43 +148,35 @@ class VigenereCipher
     reg = "(#{Regexp.union(all_words)})"
     poss_keys_and_msgs = {}
 
-
     (0..6).to_a.each do |num|
-      check_values_proc = Proc.new do |word| 
-        keys_and_msgs.values.grep(/^#{word}#{reg*num}$/).each {|val| poss_keys_and_msgs[keys_and_msgs.key(val)] ||= val } 
-      end
-
-      all_words.reject! { |beg| messages.grep(/^(#{beg})#{reg*num}/).empty? }
+      all_words.reject! { |beg| messages.grep(/^(#{beg})#{reg * num}/).empty? }
       if all_words.count > 1
-        temp = all_words.reject { |beg| messages.grep(/^(#{beg})#{reg*num}$/).empty? }
-        temp ? temp.each(&check_values_proc) : next
+        temp = all_words.reject { |beg| messages.grep(/^(#{beg})#{reg * num}$/).empty? }
+        temp ? temp.each(&check_values_proc(keys_and_msgs, reg, num, poss_keys_and_msgs)) : next
       else
-        all_words.each(&check_values_proc)
+        all_words.each(&check_values_proc(keys_and_msgs, reg, num, poss_keys_and_msgs))
       end
     end
 
     poss_keys_and_msgs
   end
-
   end
 end
 
+class Dictionary
+  EXTRA_WORDS = %w(a i is as at be ed he ho in it jo jr la re ok mo mr si sr st
+                   no my me ma if hi ha go ex do db by ax an of oh or ow ox pi
+                   so to uh um up us vs we yo)
 
-  class Dictionary
-
-    EXTRA_WORDS = %w(a i is as at be ed he ho in it jo jr la re ok mo mr si sr st no my me ma if 
-                     hi ha go ex do db by ax an of oh or ow ox pi so to uh um up us vs we yo)
-
-    def initialize(file)
-      @word_list = File.read(file).downcase
-    end
-
-    def all_words
-      @all_words ||= (@word_list.scan(/^\w{3,}$/) + EXTRA_WORDS).uniq 
-    end
-
-    def words_by_size(min, max)
-      @word_list.scan(/^\w{#{min},#{max}}$/).uniq
-    end
-
+  def initialize(file)
+    @word_list = File.read(file).downcase
   end
+
+  def all_words
+    @all_words ||= (@word_list.scan(/^\w{3,}$/) + EXTRA_WORDS).uniq
+  end
+
+  def words_by_size(min, max)
+    (@word_list.scan(/^\w{#{min},#{max}}$/)) .uniq
+  end
+end
--- vigenere-cipher_spec.rb
+++ vigenere-cipher_spec.rb
@@ -1,39 +1,39 @@
 require_relative 'vigenere-cipher'
 
-describe "VigenereCipher" do
+describe 'VigenereCipher' do
   DICTIONARY_FILE = 'words'
-  let(:key_single_letter) { "r" }
-  let(:message_single_letter) { "t" }
-	let(:key) { "reddit" }
-	let(:message) { "todayismybirthday" }
-  let(:code) { "ksgdgbjqbeqkklgdg" }
-	let(:key2) { "kurtrussell" }
-	let(:message2) { "breakdownwasawesome" }
-  let(:code2)    { "llvtbxgorhlcunxjiew" }
-  let(:key3) { "aazzaazzxx" }
-  let(:message3) { "zzjzaajzzyxppq" }
-  let(:code3) { "zziyaaiywvxpop" }
-  let(:key4) { "day" }
-  let(:message4) { "welcometoproblemoftheday" }
-  let(:code4) { "zejfokhtmsrmelcpodwhcgaw" }
+  let(:key_single_letter) { 'r' }
+  let(:message_single_letter) { 't' }
+  let(:key) { 'reddit' }
+  let(:message) { 'todayismybirthday' }
+  let(:code) { 'ksgdgbjqbeqkklgdg' }
+  let(:key2) { 'kurtrussell' }
+  let(:message2) { 'breakdownwasawesome' }
+  let(:code2)    { 'llvtbxgorhlcunxjiew' }
+  let(:key3) { 'aazzaazzxx' }
+  let(:message3) { 'zzjzaajzzyxppq' }
+  let(:code3) { 'zziyaaiywvxpop' }
+  let(:key4) { 'day' }
+  let(:message4) { 'welcometoproblemoftheday' }
+  let(:code4) { 'zejfokhtmsrmelcpodwhcgaw' }
 
-  describe "Setter Methods" do
-    before(:each) { @cipher = VigenereCipher.new } 
+  describe 'Setter Methods' do
+    before(:each) { @cipher = VigenereCipher.new }
 
-    describe "#key and #message" do
+    describe '#key and #message' do
       before do
         @cipher.key = key
         @cipher.message = message
       end
 
-      it "parses the #key and #message to only include letters" do
+      it 'parses the #key and #message to only include letters' do
         @cipher.key = "a&&*()(&^%$$#%$%&*(*aaaa11223344''''''''aabbcc"
         @cipher.message = "eebbccdd039324834;;.,..//\//r"
-        expect(@cipher.key).to eq("aaaaaaabbcc")
-        expect(@cipher.message).to eq("eebbccddr")
+        expect(@cipher.key).to eq('aaaaaaabbcc')
+        expect(@cipher.message).to eq('eebbccddr')
       end
 
-      it "nils @code if changing an instance variable after @key and @message are set" do
+      it 'nils @code if changing an instance variable after @key and @message are set' do
         @cipher.message = message2
         expect(@cipher.instance_variable_get(:@code)).to be_nil
       end
@@ -41,86 +41,86 @@ describe "VigenereCipher" do
     end
   end
 
-  describe "Getter Methods" do
-    describe "#key" do
+  describe 'Getter Methods' do
+    describe '#key' do
 
-      context "when key is present" do
+      context 'when key is present' do
         before do
           @cipher = VigenereCipher.new(key2)
         end
 
-        it "returns the set value" do
+        it 'returns the set value' do
           expect(@cipher.key).to eq(key2)
         end
       end
 
-      context "when key is nil and message and code are present" do
+      context 'when key is nil and message and code are present' do
         before do
           @cipher = VigenereCipher.new(nil, message2)
           @cipher.code = code2
         end
-        it "returns the calculated value for the key" do
-          expect(@cipher.key).to eq(VigenereCipher.cycle(key2, message2) )
+        it 'returns the calculated value for the key' do
+          expect(@cipher.key).to eq(VigenereCipher.cycle(key2, message2))
         end
 
       end
     end
 
-    describe "#message" do
+    describe '#message' do
       before(:each) do
         @cipher = VigenereCipher.new(key2, nil)
       end
 
-      context "when message is present" do
+      context 'when message is present' do
         before { @cipher.message = message2 }
-        it "returns the set value" do
+        it 'returns the set value' do
           expect(@cipher.message).to eq(message2)
         end
       end
 
-      context "when message is nil and key and code are present" do
+      context 'when message is nil and key and code are present' do
         before do
           @cipher.key = key2
           @cipher.code = code2
         end
-        
-        it "returns the calculated value" do
+
+        it 'returns the calculated value' do
           expect(@cipher.message = message2)
         end
       end
     end
 
-    describe "#code" do
+    describe '#code' do
       before(:each) do
         @cipher = VigenereCipher.new
       end
 
-      context "when key and message are present" do
+      context 'when key and message are present' do
         before do
           @cipher.key = key2
           @cipher.message = message2
         end
-        
-        it "returns the calculated value" do
+
+        it 'returns the calculated value' do
           expect(@cipher.code).to eq(code2)
         end
       end
 
-      context "when key and message are nil" do
-        it "asks for the user to enter a key and message" do
+      context 'when key and message are nil' do
+        it 'asks for the user to enter a key and message' do
           @cipher.stub(:gets).and_return("#{key2}\n", "#{message2}\n")
           expect(@cipher.key).to eq(key2)
           expect(@cipher.message).to eq(message2)
           expect(@cipher.code).to eq(code2)
         end
       end
-      
-      context "when key is present and message is nil" do
+
+      context 'when key is present and message is nil' do
         before do
           @cipher.key = key2
         end
 
-        it "asks for the user to enter a message" do
+        it 'asks for the user to enter a message' do
           @cipher.stub(:gets).and_return("#{message2}\n")
           expect(@cipher.key).to eq(key2)
           expect(@cipher.message).to eq(message2)
@@ -128,12 +128,12 @@ describe "VigenereCipher" do
         end
       end
 
-      context "when key is nil and message is present" do
+      context 'when key is nil and message is present' do
         before do
           @cipher.message = message2
         end
 
-        it "asks for the user to enter a key" do
+        it 'asks for the user to enter a key' do
           @cipher.stub(:gets).and_return("#{key2}\n")
           expect(@cipher.key).to eq(key2)
           expect(@cipher.message).to eq(message2)
@@ -143,30 +143,30 @@ describe "VigenereCipher" do
     end
   end
 
-  describe "Class methods" do
-    describe ".cycle" do
+  describe 'Class methods' do
+    describe '.cycle' do
       let(:response) { VigenereCipher.cycle(key, message) }
-      
-      it "cycles through the key until its length == message.length" do
-        expect(response).to eq ("redditredditreddi")
+
+      it 'cycles through the key until its length == message.length' do
+        expect(response).to eq('redditredditreddi')
       end
     end
 
-    describe ".get_code" do
+    describe '.get_code' do
       subject { VigenereCipher.get_code(key, message) }
-      it {should eq(code) }
+      it { should eq(code) }
     end
 
-    describe ".get_message_or_key KEY" do
-      it "should return the proper key" do
+    describe '.get_message_or_key KEY' do
+      it 'should return the proper key' do
         expect(VigenereCipher.get_message_or_key(message, code)).to eq(VigenereCipher.cycle(key, code))
         expect(VigenereCipher.get_message_or_key(message2, code2)).to eq(VigenereCipher.cycle(key2, code2))
         expect(VigenereCipher.get_message_or_key(message3, code3)).to eq(VigenereCipher.cycle(key3, code3))
       end
     end
 
-    describe ".get_message_or_key MESSAGE" do
-      it "should return the proper message" do
+    describe '.get_message_or_key MESSAGE' do
+      it 'should return the proper message' do
         expect(VigenereCipher.get_message_or_key(key, code)).to eq(message)
         expect(VigenereCipher.get_message_or_key(key2, code2)).to eq(message2)
         expect(VigenereCipher.get_message_or_key(key3, code3)).to eq(message3)
@@ -174,21 +174,21 @@ describe "VigenereCipher" do
     end
   end
 
-  describe "Cracking methods" do
-    describe ".crack" do
-      it "returns a message" do
-        expect(VigenereCipher.crack(code4, 12)).to eq({ key4 => message4 })
-        expect(VigenereCipher.crack(code2, 12)).to eq({ key2 => message2 })
-        expect(VigenereCipher.crack(code, 12)).to eq({ key => message })
-      end 
+  describe 'Cracking methods' do
+    describe '.crack' do
+      it 'returns a message' do
+        expect(VigenereCipher.crack(code4, 12)).to eq(key4 => message4)
+        expect(VigenereCipher.crack(code2, 12)).to eq(key2 => message2)
+        expect(VigenereCipher.crack(code, 12)).to eq(key => message)
+      end
     end
 
-    describe ".words_and_messages" do
-      code =  "llvtbxgorhlcunxjiew"
+    describe '.words_and_messages' do
+      code =  'llvtbxgorhlcunxjiew'
       dictionary = Dictionary.new(DICTIONARY_FILE)
       before(:all) { @words_and_messages = VigenereCipher.words_and_messages(code, dictionary, 10) }
-      
-      it "creates a hash" do
+
+      it 'creates a hash' do
         expect(@words_and_messages).to be_a Hash
       end
 
@@ -196,19 +196,29 @@ describe "VigenereCipher" do
   end
 end
 
-describe "Dictionary" do
+describe 'Dictionary' do
   before(:all) { @words = Dictionary.new(DICTIONARY_FILE) }
   subject { @words }
 
-  describe "#all_words" do
+  describe '#all_words' do
     subject { @words.all_words }
     it { should be_a Array }
     its(:count) { should eq(File.read(DICTIONARY_FILE).scan(/^\w{3,}$/).count + Dictionary::EXTRA_WORDS.count) }
+
+    it 'does not return any strings less than 3 characters' do
+      expect((@words.all_words.grep(/^\w{1,2}$/) - Dictionary::EXTRA_WORDS).count).to eq(0)
+    end
+
   end
 
-  describe "#words_by_size" do
-    subject { @words.words_by_size(3,25) }
+  describe '#words_by_size' do
+    subject { @words.words_by_size(3, 25) }
     it { should be_a Array }
-    its(:count) { should eq(File.read(DICTIONARY_FILE).scan(/^\w{3,}$/).count) }
+    its(:count) { should eq((File.read(DICTIONARY_FILE).scan(/^\w{3,}$/)) .count) }
+
+    it 'does not return any strings less than 3 characters' do
+      x = 3
+      expect(@words.words_by_size(x, 10).grep(/^\w{1,#{x - 1}}$/).count).to eq(0)
+    end
   end
 end
