495a35cf7ca334d0247458f4c7b1694d50384e47
Dom Leone <ohheydom@gmail.com>
2014-06-12 00:01:36 -0400

    Added attr_reader to remove instance variable from methods.

--- dictionary.rb
+++ dictionary.rb
@@ -3,15 +3,16 @@ class Dictionary
                    no my me ma if hi ha go ex do db by ax an of oh or ow ox pi
                    so to uh um up us vs we yo)
 
+  attr_reader :word_list
   def initialize(file)
     @word_list = File.read(file).downcase
   end
 
   def all_words
-    @all_words ||= (@word_list.scan(/^\w{3,}$/) + EXTRA_WORDS).uniq
+    @all_words ||= (word_list.scan(/^\w{3,}$/) + EXTRA_WORDS).uniq
   end
 
   def words_by_size(min, max)
-    (@word_list.scan(/^\w{#{min},#{max}}$/)).uniq
+    (word_list.scan(/^\w{#{min},#{max}}$/)).uniq
   end
 end
